286b10ff2ff55402f453cdbadc60821ff171ca8d
Sergio M <sergiomorenomartinez544@gmail.com>
2024-03-05 13:29:53 +0100

    Added MainController to API (#6)
    * Added MainController
    
    Implemented endpoint for Sum
    
    * Added subtract endpoint

---
+++ API/Controllers/MainController.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase {
+        
+        private readonly IHttpClientFactory _clientFactory;
+
+        public MainController(IHttpClientFactory httpClientFactory) {
+            _clientFactory = httpClientFactory;
+        }
+
+        [HttpPost("Sum")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Result))]
+        public async Task<IActionResult> Sum([FromBody] Problem problem) {
+            var client = _clientFactory.CreateClient();
+            var sumServiceUrl = "http://sum-service:80";
+            
+            var jsonRequest = JsonSerializer.Serialize(problem);
+            var content = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
+            
+            var response = await client.PostAsync($"{sumServiceUrl}/Sum", content);
+            
+            if (response.IsSuccessStatusCode) {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<Result>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return Ok(result);
+            } 
+            
+            return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+        }
+        
+        [HttpPost("Subtract")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Result))]
+        public async Task<IActionResult> Subtract([FromBody] Problem problem) {
+            var client = _clientFactory.CreateClient();
+            var sumServiceUrl = "http://subtract-service:80";
+            
+            var jsonRequest = JsonSerializer.Serialize(problem);
+            var content = new StringContent(jsonRequest, System.Text.Encoding.UTF8, "application/json");
+            
+            var response = await client.PostAsync($"{sumServiceUrl}/Subtract", content);
+            
+            if (response.IsSuccessStatusCode) {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<Result>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return Ok(result);
+            } 
+            
+            return StatusCode((int)response.StatusCode, response.ReasonPhrase);
+        }
+    }
+}
